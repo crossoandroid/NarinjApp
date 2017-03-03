9779d9040cfeaca97e3b54e5d49f9f0082b25a6d
Hayk <qerobyan.hayk@gmail.com>
2017-03-03 19:55:11 +0400

    Enum added

---
+++ app/src/main/java/com/orange_team/narinjapp/enums/OrderCategories.java
@@ -0,0 +1,20 @@
+package com.orange_team.narinjapp.enums;
+
+/**
+ * Created by Hayk on 02-Mar-17.
+ */
+
+public enum OrderCategories {
+
+        SOUP,                 //apur
+        SALAD,                //axcanner
+        RECEPTION,            //furshetner
+        LUNCH,                //lunch
+        CAKE,                 //txvacqner
+        MEAL,                 //utestner
+        GARNISH,              //xavartner
+        COOKS,                //xohararner,
+        ALL;                  //bolory
+
+
+}
