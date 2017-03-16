dd6b33c62682d1b74dcd24447356d0224c43d84e
Khachatur <kirakosyankhachatur@gmail.com>
2017-03-16 18:35:42 +0400

    Added AlertDialogManager

---
+++ app/src/main/java/com/orange_team/narinjapp/utils/AlertDialogManager.java
@@ -0,0 +1,34 @@
+package com.orange_team.narinjapp.utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.orange_team.narinjapp.R;
+
+public class AlertDialogManager {
+	@SuppressWarnings("deprecation")
+	public void showAlertDialog(Context context, String title, String message, Boolean status) {
+		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+		alertDialog.setTitle(title);
+
+		alertDialog.setMessage(message);
+
+		if (status != null)
+			alertDialog.setIcon((status) ? R.mipmap.ic_launcher : R.mipmap.ic_launcher);
+
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+			}
+
+			private void finish() {
+
+			}
+		});
+
+		alertDialog.show();
+	}
+}
